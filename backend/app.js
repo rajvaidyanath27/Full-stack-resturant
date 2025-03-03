fe86beab8fe56c7c512168751f57498eca99f1d8
Raj <rajbaidyanath76@gmail.com>
2025-03-03 23:52:11 +0530

    h

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {dbConnection} from "./db/dbConnection.js";
+import rootUser from "./routes/routeUser.js";
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
@@ -17,6 +18,10 @@ app.use(
         credentials: true
     })
 );
+
+app.use('./api/reservation', rootUser);
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));

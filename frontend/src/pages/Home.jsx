795b6121901f6b65f80ca24bab84244fb60ff806
Raj <rajbaidyanath76@gmail.com>
2025-03-04 23:50:21 +0530

    l

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import About from '../components/About';
+import Quality from '../components/Quality';
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection />
+    <About />
+    <Quality />
+    </>
+  )
+}
+
+export default Home

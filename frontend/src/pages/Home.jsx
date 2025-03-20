abb1593eabc0c9172fb6b0b9ddd35f0d6d56c634
Raj <rajbaidyanath76@gmail.com>
2025-03-20 23:58:05 +0530

    rtt

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import About from '../components/About';
 import Quality from '../components/Quality';
-import WhoAreWe from '../components/WhoareWe';
+import WhoAreWe from '../components/WhoAreWe';
 import Team from '../components/Team';
 import Footer from '../components/Footer';
 import Reservation from '../components/Reservation';
